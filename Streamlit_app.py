8c9f3f4f0cde494a140c480d2841785277ebe7bc
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-12-16 14:42:39 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -7,15 +7,16 @@ streamlit.title('Air Canada Webpage')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #sets the index to Fruit column NOT NUMBERS
-my_fruit_list = my_fruit_list.set_index()
+my_fruit_list = my_fruit_list.set_index('Roles')
 #pick select list here for smothie creation: (list from index index declared)
 #manually adds avo and bana to list 
-fruits_selected = streamlit.multiselect("Select a Role")
+fruits_selected = streamlit.multiselect("Select a role", list(my_fruit_list.index),['',''])
 #hides the giant table 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display table on page (was my fruit list) now just fruits to show 
 streamlit.dataframe(fruits_to_show)
 #create the repeatable code block (called a function) function starts with def: and returns:
+
 
 streamlit.header('Current Privileges: ' )
 streamlit.header('Roles with correct Privileges: ')
