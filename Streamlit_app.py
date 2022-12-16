9e0209a9a85c13cc0d9c23480b2daac1d328c8ee
django <108687989+j-isaac-melle@users.noreply.github.com>
2022-12-16 14:43:00 -0500

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -7,10 +7,10 @@ streamlit.title('Air Canada Webpage')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 #sets the index to Fruit column NOT NUMBERS
-my_fruit_list = my_fruit_list.set_index('Roles')
+my_fruit_list = my_fruit_list.set_index('Fruit')
 #pick select list here for smothie creation: (list from index index declared)
 #manually adds avo and bana to list 
-fruits_selected = streamlit.multiselect("Select a role", list(my_fruit_list.index),['',''])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 #hides the giant table 
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 #display table on page (was my fruit list) now just fruits to show 
